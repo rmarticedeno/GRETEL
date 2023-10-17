82457399feafde91ec57facfe46a1da757db2532
rmarticedeno <rmarticedeno@gmail.com>
2023-10-17 03:48:53 -0400

    Diameter manipulator

---
+++ src/n_dataset/manipulators/diameter.py
@@ -0,0 +1,24 @@
+import networkx as nx
+import numpy as np
+
+from src.n_dataset.manipulators.base import BaseManipulator
+
+
+class Diameter(BaseManipulator):
+    def graph_info(self, instance):
+        graph = instance._build_nx()
+
+        diameter = 0
+        if nx.is_connected(graph):
+            diameter = nx.diameter(graph)
+        else:
+            components = nx.connected_components(graph)
+            largest_component = max(components, key=len)
+            subgraph = graph.subgraph(largest_component)
+            diameter = nx.diameter(subgraph)
+        
+        feature_map = {
+            "diameter": diameter
+        }
+        return feature_map
+    
